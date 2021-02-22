9f47774a31dcc251387c55bb94cc39a8a88bada8
Healops <52401794+Healops@users.noreply.github.com>
2021-02-23 02:06:50 +0700

    Update Scan.sh

--- Scripts/Scan.sh
+++ Scripts/Scan.sh
@@ -1,5 +1,6 @@
-!/bin/bash
+#!/bin/bash
 masscan --rate=3000 -e tun0 -p1-65535 $1 > $2
+mkdir Scan-results
 mkdir Scan-results/$2
 cat $2 | awk -F " " '{print $4}' | awk -F "/" '{print $1}' | sort -n | tr "\n" ',' | sed 's/,$//' > Scan-results/$2/$2-ports
 rm $2
