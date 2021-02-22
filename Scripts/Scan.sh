a3d0588bcfe2b47b5e2ce2efb086c9d969c1391f
Healops <52401794+Healops@users.noreply.github.com>
2021-02-23 00:58:33 +0700

    Create Scan.sh

---
+++ Scripts/Scan.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+masscan --rate=3000 -e tun0 -p1-65535 $1 > $2
+mkdir Scan-results/$2
+cat $2 | awk -F " " '{print $4}' | awk -F "/" '{print $1}' | sort -n | tr "\n" ',' | sed 's/,$//' > Scan-results/$2/$2-ports
+rm $2
+ports=`cat Scan-results/$2/$2-ports`
+nmap -T4 -Pn -sV -sC -oA Scan-results/$2/$2-version $1 -p$ports
+rm Scan-results/$2/$2-version.xml
+rm Scan-results/$2/$2-version.gnmap
